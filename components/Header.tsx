1e28e6a5a8631b03cebc5660f702317d94707469
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2023-11-18 16:53:59 +1100

    edit

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps{
     children: React.ReactNode;
@@ -89,7 +90,11 @@ const Header: React.FC<HeaderProps> =({
             items-center
             gap-x-4">
                 <>
-                
+                <div>
+                    <Button>
+                        Sign up
+                    </Button>
+                </div>
                 </>
             </div>
 
